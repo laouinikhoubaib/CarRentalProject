1e6a40db01b23e24679665734ac53367417fbf75
laouinikhoubaib <73175459+laouinikhoubaib@users.noreply.github.com>
2023-05-16 14:11:20 +0100

    ajout de réservation entity

---
+++ src/main/java/com/example/carrental/DTO/VehiculeDTO.java
@@ -0,0 +1,42 @@
+package com.example.carrental.DTO;
+
+
+import com.example.carrental.Enumerations.Alimentation;
+import com.example.carrental.Enumerations.EtatActuel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class VehiculeDTO {
+
+    private Integer vehiculeId;
+
+    @Size(max = 255)
+    private String matricule;
+    @Size(max = 255)
+    private String nbr_places;
+    @Size(max = 255)
+    private String couleur;
+    @Size(max = 255)
+    private String longueur;
+    @Size(max = 255)
+    private String largeur;
+    @Size(max = 255)
+    private String puissance;
+    @Size(max = 255)
+    private String charge_utile;
+    @Size(max = 255)
+    private String picture;
+
+    private LocalDate dateajout;
+
+    private Double jourslocation;
+
+    private Alimentation alimentation;
+
+    private EtatActuel etatactuel;
+}
--- src/main/java/com/example/carrental/Models/Vehicule.java
+++ src/main/java/com/example/carrental/Models/Vehicule.java
@@ -4,11 +4,13 @@ package com.example.carrental.Models;
 import com.example.carrental.Enumerations.Alimentation;
 import com.example.carrental.Enumerations.EtatActuel;
 import com.example.carrental.Enumerations.TypeCategorie;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Set;
 
 @Getter
@@ -22,47 +24,58 @@ import java.util.Set;
 public class Vehicule {
 
     @Id
+    @Column(nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long vehiculeId;
+    private Integer vehiculeId;
 
     @Column(name = "matricule",  nullable = false, length = 100)
-    String matricule;
+    private String matricule;
 
     @Column(name = "nbr_places", nullable = false, length = 100)
-    String nbr_places;
+    private String nbr_places;
 
     @Column(name = "couleur", nullable = false, length = 100)
-    String couleur;
+    private String couleur;
 
     @Column(name = "longueur", nullable = false, length = 100)
-    String longueur;
+    private String longueur;
 
     @Column(name = "largeur",  nullable = false, length = 100)
-    String largeur;
+    private String largeur;
 
     @Column(name = "puissance", nullable = false, length = 100)
-    String puissance;
+    private String puissance;
 
     @Column(name = "charge_utile", nullable = false, length = 100)
-    String charge_utile;
+    private String charge_utile;
+
+    @Column
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dateajout;
+
+    @Column
+    private String picture;
+
+    @Column
+    private Double jourslocation;
+
 
     @Enumerated(EnumType.STRING)
-    Alimentation alimentation;
+    private Alimentation alimentation;
 
     @Enumerated(EnumType.STRING)
-    EtatActuel etatactuel;
+    private EtatActuel etatactuel;
+
+
 
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL ,mappedBy = "vehicule")
     Set<ModelVehicule> modelVehicules;
 
+    @OneToMany(mappedBy = "vehiculeReservation")
+    private Set<Reservation> vehiculeReservationReservations;
 
-    @OneToOne
-    Media vehiculepicture;
-
-    @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", referencedColumnName = "userId")
+    @ManyToOne
     private User user;
 
     @JsonIgnore
