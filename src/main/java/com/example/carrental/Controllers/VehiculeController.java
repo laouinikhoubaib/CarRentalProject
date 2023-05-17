ac9bfbe4202a5010028304af8ae69b9eebcb6f3e
laouinikhoubaib <73175459+laouinikhoubaib@users.noreply.github.com>
2023-05-17 15:26:25 +0100

    ajout des méthodes pour vehiculeServices

--- src/main/java/com/example/carrental/Controllers/VehiculeController.java
+++ src/main/java/com/example/carrental/Controllers/VehiculeController.java
@@ -2,6 +2,7 @@ package com.example.carrental.Controllers;
 
 
 import com.example.carrental.DTO.VehiculeDTO;
+import com.example.carrental.Models.User;
 import com.example.carrental.Models.Vehicule;
 import com.example.carrental.Repository.UserRepository;
 import com.example.carrental.Repository.VehiculeRepository;
@@ -11,7 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -80,6 +84,102 @@ public class VehiculeController {
             return ResponseEntity.ok(vehiculeDTO);
         } else {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/Disponibilite/{vehiculeid}")
+    public ResponseEntity<List<String>> getVehicule(@PathVariable("vehiculeid") int vehiculeid){
+        List<String> disponibilite = vehiculeServiceimpl.vehiculesdisponible(vehiculeid);
+        if (disponibilite.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            String message = "Disponibilité de la vehicule avec l'id " + vehiculeid;
+            return ResponseEntity.ok().body(disponibilite);
+        }
+    }
+    @GetMapping("/getNTopRentalOffers/{vehicule}")
+    public ResponseEntity<?> getRentalOffersByRangePrice(@PathVariable("vehicule") Integer vehicule){
+        List<VehiculeDTO> vehiculeDTOList = vehiculeService.findTopNByOrderByVehiculedateDesc(vehicule);
+        if (!vehiculeDTOList.isEmpty()) {
+            return ResponseEntity.ok(vehiculeDTOList);
+        } else {
+            String errorMessage = "Aucune offre de location trouvée pour le nombre demandé de " + vehicule;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        }
+    }
+
+    @GetMapping("/getRentalOffersByRangePrice/{prix1}/{prix2}")
+    public ResponseEntity<List<VehiculeDTO>> getRentalOffersByRangePrice(@PathVariable("prix1")double prix1,@PathVariable("prix2")double prix2){
+        List<VehiculeDTO> vehicules = vehiculeService.getVehiculesByRangePrix(prix1,prix2);
+        if (vehicules.isEmpty()) {
+            String message = "Il n'existe pas des véhicules de location entre les prix " + prix1 + " et " + prix2;
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            String message = "Liste des véhicules de location entre les prix " + prix1 + " et " + prix2;
+            return ResponseEntity.ok().body(vehicules);
+        }
+    }
+
+    @GetMapping("/tri")
+    public ResponseEntity<List<VehiculeDTO>> getRentalOffersOrderByPriceAsc() {
+        List<VehiculeDTO> vehicules = vehiculeService.findAllOrderByPrixAsc();
+        if (vehicules.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            String message = "Liste des véhicules de location triées par prix en ordre ascendant récupérée avec succès";
+            return ResponseEntity.ok().header("message", message).body(vehicules);
+        }
+    }
+
+    @GetMapping("/revenueoffer/{vehiculeId}")
+    public ResponseEntity<Object> calculateRevenueForUser(@PathVariable int vehiculeId) {
+        Double revenue = vehiculeService.getChiffreAffaireByVehicule(vehiculeId);
+        if (revenue == null) {
+            String message = "L'offre de location avec l'ID " + vehiculeId + " n'existe pas";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        } else {
+            String message = "Le chiffre d'affaires pour l'offre de location avec l'ID " + vehiculeId + " est de " + revenue;
+            return ResponseEntity.ok().body(message);
+        }
+    }
+
+    @PostMapping("/addImage/{userId}")
+    public ResponseEntity<?> addRentalOffer(@RequestParam("title") String title,
+                                            @RequestParam(value = "image", required = false) MultipartFile image,
+                                            @RequestParam("jourslocation") Double jourslocation,
+                                            @RequestParam("adress") String adress,
+                                            @RequestParam("description") String description,
+                                            @PathVariable("userId") int userId) throws IOException {
+
+        // Vérifier si l'utilisateur existe
+        User user = userRepository.findById((long) userId).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Utilisateur non trouvé");
+        }
+
+        // Créer un nouveau plan
+        Vehicule vehicule = new Vehicule();
+        vehicule.setUser(user);
+//        vehicule.setTitle(title);
+//        vehicule.setOffredate(LocalDate.now());
+//        vehicule.setDescription(description);
+//        vehicule.setMonthlyrent(monthlyrent);
+//        vehicule.setAdress(adress);
+
+        // Enregistrer l'image si elle existe
+        if (image != null && !image.isEmpty()) {
+            String imagePath = vehiculeServiceimpl.saveImage(image,vehicule);
+            vehicule.setPicture(imagePath);
+        }
+
+        // Enregistrer le plan
+        int rentalOfferid = vehiculeService.addVehicule(vehicule, userId);
+
+        if (rentalOfferid > 0) {
+            return ResponseEntity.ok().body(vehiculeRepository.findById(rentalOfferid).orElse(null));
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erreur lors de l'ajout de l'offre de location");
         }
     }
 }
