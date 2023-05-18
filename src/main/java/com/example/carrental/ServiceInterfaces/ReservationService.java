0ddfe9eb30dc99e367f04d5055ad68a005f485c5
laouinikhoubaib <73175459+laouinikhoubaib@users.noreply.github.com>
2023-05-18 15:03:06 +0100

    ajout des crud pour reservation service

--- src/main/java/com/example/carrental/ServiceInterfaces/ReservationService.java
+++ src/main/java/com/example/carrental/ServiceInterfaces/ReservationService.java
@@ -1,6 +1,7 @@
 package com.example.carrental.ServiceInterfaces;
 
 import com.example.carrental.DTO.ReservationDTO;
+import com.example.carrental.Models.Reservation;
 
 import java.util.List;
 
@@ -8,4 +9,6 @@ public interface ReservationService {
 
     public List<ReservationDTO> findAll();
     public ReservationDTO getById(final Integer reservid);
+    boolean updateReservation(Reservation reservation);
+    boolean deleteReservation(int id);
 }
